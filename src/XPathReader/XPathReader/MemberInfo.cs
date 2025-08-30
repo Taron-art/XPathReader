ce7682a4046316d0830d8ccd297eb456e2d84632
Artur Kharin <kharin.art@gmail.com>
2025-08-30 18:04:19 +0300

    9 - Added base source generation code

---
+++ src/XPathReader/XPathReader/MemberInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+namespace XPathReader
+{
+    internal sealed record MemberInfo(string Keyword, string Namespace, string Name)
+    {
+        public MemberInfo? Parent { get; set; }
+    }
+}
---
+++ src/XPathReader/XPathReader/XPathReaderDataToGenerate.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace XPathReader
+{
+    internal sealed record XPathReaderDataToGenerate(MemberInfo MethodInfo, bool IsProperty, Location DiagnosticLocation, string MemberName, string Modifiers, bool IsNullable, string XPaths, CompilationData CompilationData)
+    {
+        public string? GeneratedName { get; set; }
+    }
+}
