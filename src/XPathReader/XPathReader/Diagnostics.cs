1f015656e5df9652794d6216f42bf5625db1b40f
Artur Kharin <kharin.art@gmail.com>
2025-08-31 12:04:12 +0300

    12 - Added license check on Windows.

---
+++ src/XPathReader/XPathReader/Diagnostics.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace XPathReader
+{
+    internal static class Diagnostics
+    {
+        public static readonly DiagnosticDescriptor LicenseViolation = new(
+            id: "XPR001",
+            title: "Possible license agreement violation detected",
+            messageFormat: "Please read the license agreement. Code won't be emmitted.",
+            category: "Usage",
+            defaultSeverity: DiagnosticSeverity.Error,
+            isEnabledByDefault: true
+        );
+    }
+}
