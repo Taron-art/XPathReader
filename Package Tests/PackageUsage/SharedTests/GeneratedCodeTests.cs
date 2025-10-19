fbe0333184e4c85a2982e6312ab8fb3584798825
Artur Kharin <kharin.art@gmail.com>
2025-10-19 12:16:56 +0300

    35 - Add release build with NuGet push and Test project to test before. (#36)
    - Added test project to locally test NuGet package before release;
    - Added release build with NuGet push

---
+++ Package Tests/PackageUsage/SharedTests/GeneratedCodeTests.cs
@@ -0,0 +1,27 @@
+
+using System;
+using ARTX.XPath;
+using NUnit.Framework;
+
+#nullable enable
+
+namespace PackageUsage.Tests
+{
+
+    [TestFixture]
+    public partial class GeneratedCodeTests
+    {
+        [GeneratedXPathReader("/bookstore/title")]
+        public partial XPathReader GetReader();
+
+        [Test]
+        public void XPathReader_GetReader_ReturnsReaderOfCurrentVersion()
+        {
+            Version? testAssemblyVersion = GetType().Assembly.GetName().Version;
+
+            Version? readerAssemblyVersion = GetReader().GetType().Assembly.GetName().Version;
+
+            Assert.That(testAssemblyVersion, Is.EqualTo(readerAssemblyVersion), "The generated code is not using the expected version of ARTX.XPath.");
+        }
+    }
+}
